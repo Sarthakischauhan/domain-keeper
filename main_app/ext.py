290675ed30925821b18bcf5ad0b96d3426fc7d64
sarthakischauhan18 <sarthakchauhann@gmail.com>
2020-12-01 23:05:11 +0530

    added migrate class

--- main_app/ext.py
+++ main_app/ext.py
@@ -2,9 +2,10 @@ from main_app import app
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
+from flask_migrate import Migrate
 
 #initializing the dependency
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 bcrypt = Bcrypt(app)
+migrate = Migrate(app,db)
