d3ffab9a63a29e7ad5dd5860dfcf0f9cd6e18f52
sarthakischauhan18 <sarthakchauhann@gmail.com>
2020-12-01 21:36:23 +0530

    Login logic and page build

--- main_app/auth.py
+++ main_app/auth.py
@@ -1,7 +1,8 @@
-from flask import Blueprint,render_template,url_for,flash
-from main_app.forms import SignupForm
+from flask import Blueprint,render_template,url_for,flash,request
+from main_app.forms import SignupForm,LoginForm
 from main_app.models import User
 from main_app.ext import (db , bcrypt)
+from flask_login import login_user,current_user
 
 
 auth_bp = Blueprint("auth",__name__)
@@ -12,7 +13,7 @@ def sample_auth():
     return "This is a sample auth endpoint"
 
 
-@auth_bp.route("/signup/",methods=["GET","POST"])
+@auth_bp.route("/signup",methods=["GET","POST"])
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
@@ -24,6 +25,24 @@ def signup():
         return redirect(url_for("routes_bp.index"))
 
     return render_template("signup.html",form=form)
+
+
+
+@auth_bp.route("/login",methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data)
+        password_check = bcrypt.check_password_hash(User.query.password,form.password.data).first()
+        if user and password_check:
+            login_user(user)
+            next_page = request.args.get("next") 
+            return "Login Successful" if next_page else "catch me if you can"
+        else :
+            flash("Login Unsuccessfull")
+
+    return render_template("login.html",form=form)
+        
 
 
 
--- main_app/forms.py
+++ main_app/forms.py
@@ -1,21 +1,27 @@
 from flask_wtf import FlaskForm as Form
 from wtforms import StringField,PasswordField,SubmitField,HiddenField
 from wtforms.validators import DataRequired,Email,ValidationError,Length,EqualTo
-
+from main_app.models import User
 
 
 
 class SignupForm(Form):
-     username=StringField("Username",validators=[DataRequired(),Length(min=2)])
-     name = StringField("Name",validators=[DataRequired(),Length(min=6,max=20)]) 
-     email=StringField("Email",validators=[DataRequired(),Email()])
-     password=PasswordField("Password",validators=[DataRequired(),Length(min=4,max=12,message="Password should be of 4 to 12 characters")])
-     confirm=PasswordField("Confirm Password",validators=[DataRequired(),EqualTo('password',"Invalid confirm password!!")])
-     submit=SubmitField("Sign Up")
+    username=StringField("Username",validators=[DataRequired(),Length(min=2)])
+    name = StringField("Name",validators=[DataRequired(),Length(min=6,max=20)])
+    email=StringField("Email",validators=[DataRequired(),Email()])
+    password=PasswordField("Password",validators=[DataRequired(),Length(min=4,max=12,message="Password should be of 4 to 12 characters")])
+    confirm=PasswordField("Confirm Password",validators=[DataRequired(),EqualTo('password',"Invalid confirm password!!")])
+    submit=SubmitField("Sign Up")
 
-#     def validate_email(self,email):
-#        user=School.query.filter_by(email=email.data).first()
-#        if user:
-#           raise ValidationError("This Email is taken.Try a new One")
+    def validate_email(self,email):
+      user=User.query.filter_by(email=email.data).first()
+      if user:
+        raise ValidationError("This Email is taken.Try a new One")
 
+
+class LoginForm(Form):
+    username=StringField("Username",validators=[DataRequired(),Length(min=2)])
+    email=StringField("Email",validators=[DataRequired(),Email(message="Please check your email")])
+    password=PasswordField("Password",validators=[DataRequired(),Length(min=4,max=12,message="Password should be of 4 to 12 characters")])
+    submit=SubmitField("Login")
 
