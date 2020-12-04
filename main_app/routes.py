784b43fff90aee5f686c794c1bc2e1897185a901
sarthakischauhan18 <sarthakchauhann@gmail.com>
2020-12-04 13:21:29 +0530

    minor changes to improve the app

--- main_app/routes.py
+++ main_app/routes.py
@@ -1,9 +1,18 @@
 from flask import Blueprint,render_template,url_for
+from flask_login import login_required,current_user
+
 
 routes_bp = Blueprint("routes_bp",__name__)
 
 @routes_bp.route("/")
 def index():
     return render_template("home.html")
+
+
+@routes_bp.route("/<string:username>")
+@login_required
+def account(username):
+    return f"welcome {username}"
+
 
 
