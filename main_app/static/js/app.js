d9a514aa4be6f31147ce635d7ff90aba22bd5e4b
sarthakischauhan18 <sarthakchauhann@gmail.com>
2020-12-16 20:24:44 +0530

    added some more frontend features

--- main_app/static/js/app.js
+++ main_app/static/js/app.js
@@ -1,1 +1,14 @@
+function flashboxEnd(){
+    var flash_red=document.getElementById("flash_box-red");
+    var flash_green=document.getElementById("flash_box-green");
 
+    if (flash_red !== null){
+        flash_red.style.display="none";
+
+    }else if (flash_green !== null){
+        flash_green.style.display="none";
+
+    }else{
+        console.log("Nothing changed")
+    }
+}
