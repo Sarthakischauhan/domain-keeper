63c8ff6501681d709f40f3259fbdfdd0a7cddc5f
sarthakischauhan18 <sarthakchauhann@gmail.com>
2021-01-11 22:43:19 +0530

    deleted a start file

--- start.sh
+++
@@ -1,5 +0,0 @@
-export FLASK_APP="app.py"
-export FLASK_ENV=development
-export ENCRYPTION_KEY="WNZlRBRt_htXVlnxTGOKIhy47TfA1Ax4ANgINk6By-o="
-export YOUTUBE_API_KEY="AIzaSyAuDDv16SdUt6hPDr2NBq2Y8HFtJx41vcc"
-source ./venv/Scripts/activate
